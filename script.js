29f1f98316b070d4625e77108f9edfa6f1bdefc0
Chowww <67734552+chow1103@users.noreply.github.com>
2020-07-04 18:36:01 +0800

    footer

--- script.js
+++
@@ -1,9 +0,0 @@
-//Owl Carousel Plugin
-$(document).ready(function() {
-    $("#bookdocSlider").owlCarousel({
-        navigation : true, // Show next and prev buttons
-        slideSpeed : 300,
-        paginationSpeed : 400,
-        singleItem:true
-    });
-});
