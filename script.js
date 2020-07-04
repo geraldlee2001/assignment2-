c30327de2c5c45afb489a65147e3431053d39f3a
Chowww <67734552+chow1103@users.noreply.github.com>
2020-07-04 09:27:24 +0800

    experimental

---
+++ script.js
@@ -0,0 +1,9 @@
+//Owl Carousel Plugin
+$(document).ready(function() {
+    $("#bookdocSlider").owlCarousel({
+        navigation : true, // Show next and prev buttons
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true
+    });
+});
