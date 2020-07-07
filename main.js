8580f78df659dbf42486c060905104b7e2672055
geraldlee2001 <geraldlee168168@gmail.com>
2020-07-07 21:43:15 +0800

    JavaScript Testing

---
+++ main.js
@@ -0,0 +1,13 @@
+function loginCheck()
+{
+    var email;
+    email=document.getElementById("emailLogin").value.indexOf("@");
+    console.log(email);
+    document.getElementById("loginSubmit").onclick = alert("Please enter your email");;
+    function reply_click()
+    {
+
+            alert("Please enter your email");
+        
+    }
+}
