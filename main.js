8ef6c249ffc3e58adc12c9c14201036eacc3f1ee
Chowww <67734552+chow1103@users.noreply.github.com>
2020-07-09 10:36:52 +0800

    Closer to done

--- main.js
+++ main.js
@@ -13,3 +13,16 @@ function loginCheck()
         }
     }
 }
+
+//Owl Carousel Plugin
+$(document).ready(function() {
+    $("#bookdocSlider").owlCarousel({
+        autoPlay : true,
+        autoPlay: 1000,
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem: true,
+        loop:true,
+        nav:true,
+    });
+});
