076b80a88a7e32a0a61d3de96ecbaddd5dc8f06d
Lim Yi Yan <linyiyan@Lins-MacBook-Pro.local>
2020-07-12 11:41:53 +0800

    動くな数字、凄いよね？

--- main.js
+++ main.js
@@ -22,3 +22,25 @@ function marketMemberRedirect()
 {
     window.open("https://shop.bookdoc.com/","_blank");
 }
+
+const counters = document.querySelectorAll('.counter');
+const speed = 200; 
+
+counters.forEach(counter => {
+	const updateCount = () => {
+		const target = +counter.getAttribute('data-target');
+		const count = +counter.innerText;
+
+		const inc = target / speed;
+
+
+		if (count < target) {
+			counter.innerText =Math.round(count + inc);
+			setTimeout(updateCount, 1);
+		} else {
+			counter.innerText = target;
+		}
+	};
+
+	updateCount();
+});
